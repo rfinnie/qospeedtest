2be6e272c009aac0b21b525f33e830ab133f5445
Ryan Finnie <ryan@finnie.org>
2026-05-01 10:46:16 -0700

    Bump requires-python; add explicit build-system; add stub setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+# SPDX-PackageName: qospeedtest
+# SPDX-PackageSupplier: Ryan Finnie <ryan@finnie.org>
+# SPDX-PackageDownloadLocation: https://codeberg.org/rfinnie/qospeedtest
+# SPDX-FileCopyrightText: © 2026 Ryan Finnie <ryan@finnie.org>
+# SPDX-License-Identifier: MPL-2.0
+
+from setuptools import setup
+
+setup()
